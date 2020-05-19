747aee91cdbcedda66a967a9c60083f77c2fade7
danielward27 <danielward27@outlook.com>
2020-05-19 17:32:41 +0100

    Add timer app graphs and link to google sheets

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 ### Introduction
-Climbing has relatively recently picked up in popularity, being included for the first time in the Tokyo 2020 olympics. There is very little data on climbing habits of successful (and unsucessful) climbers, and training routines are generally heavily anecdotal. A reddit user, u/higiff, posted a survey on the "climb harder" subreddit (https://www.reddit.com/r/climbharder/comments/6693ua/climbharder_survey_results/), in order to question climbers about their training habits. This to my knowledge is the largest publically available dataset on climbers and their training strategies.
-
-Having been climbing consistently for around six years now, I was intrigued to see what information this dataset might contain. Causal inference is unfortunately impossible with this dataset, but nevertheless, in the absence of better data, understanding the habits of highly effective climbers is still potentially useful. I will be judging the ability of a climber on their maximum boulder grade, as it is the least subjective measure of climbing ability in the dataset and has few missing values.
+Climbing has relatively recently picked up in popularity, being included for the first time in the Tokyo 2020 Olympics. There is very little data on climbing habits of successful (and unsuccessful) climbers, and training routines are generally heavily anecdotal. A reddit user, u/higiff, posted a survey on the "climb harder" subreddit (https://www.reddit.com/r/climbharder/comments/6693ua/climbharder_survey_results/). Using this survey, I identified some of the habits of highly successful climbers, and using this information subsequently made a training app.
 
 The analysis is split into three parts, each having it's own notebook:
 - Data cleaning and basic feature extraction (in data_cleaning.ipynb)
 - Exploratory data analysis (in eda.ipynb)
 - Modelling and analysis (in analysis.ipynb)
 
-Following the analysis, I made a shiny timer app, which implements some of the training strategies which seem to be effective (from both the analysis above, and the general cencus of climbers).
-
-The app is based mostly around hangboard training strategies (which is most of what I have available during the current Covid-19 pandemic). After each excercise, the user can input their results (weight and time), which is automatically added to a google sheets, so people can track their progress.
+The app is based mostly around hangboard training strategies (which is most of what I have available during the current Covid-19 pandemic). App features include:
+- A timer that beeps to indicate the user to carry out the specified exercise.
+- An easy way to record data, which is automatically uploaded to a google sheets page.
+- Graphs of the data, so users can track their progress.
--- hangboard_timer/app.R
+++ hangboard_timer/app.R
@@ -6,113 +6,187 @@ library(shiny)
 library(lubridate)
 library(beepr)
 library(tidyverse)
+library(googlesheets4)
+library(googledrive)
 
-#### Make dataframe of exercises ####
-exercise = c("Warm Up", rep(c("Front Lever", "Front Three", "Front Three", "Half Crimp", "Half Crimp", "Pinch Block",
-                    "Pinch Block", "Half Crimp", "Half Crimp"), 2), "Face Pulls", "External Rotations", "Finished")
+theme_set(
+  theme_bw(base_size = 25)
+)
+        
+sheet_URL = "https://docs.google.com/spreadsheets/d/17qnd_BELvIaU-FIFs8tZSxY8Th0irG4Bw_CJk1Atxo8/edit?usp=sharing"
+
+# Set up exercises
+exercises = c("Warm Up", rep(c("Front Lever", "Front Three", "Front Three",
+                               "Half Crimp", "Half Crimp", "Pinch Block",
+                              "Pinch Block", "Half Crimp", "Half Crimp"), 2),
+              "Face Pulls", "External Rotations", "Finished")
+
+# Exercises we want to record data for
+tracked_exercises = choices = c("Front Lever", "Front Three",
+                                "Front Three", "Half Crimp",
+                                "Half Crimp", "Pinch Block")
 
 bh = 20 # Time between left and right hangs
 hand = c("B", "B", rep(c("R", "L", "L", "R"), 2), "B", rep(c("L", "R", "R", "L"), 2), "B", "B", "B")
 hand[hand == "B"] <- "Both"
 hand[hand == "L"] <- "Left"
 hand[hand == "R"] <- "Right"
-                                           
-df = tibble("exercise" = exercise, "hand" = hand,
-            "timings" = NA, "cum_timings" = NA, "record_time" = NA, "record_weight" = NA,
-            "date" = as.character(Sys.Date()))
 
-
+#### UI ####
 ui <- fluidPage(
-  titlePanel("Hangboard Timer"),
-  hr(),
-  numericInput('interval','Time between exercise / s:', value=240, min=0, max=99999, step=1),
-  actionButton('start','Start'),
-  actionButton('stop','Stop'),
-  actionButton('reset','Reset'),
-  textOutput('next_ex_title'),
-  textOutput('next_ex'),
-  textOutput('time_to_ex'),
-  tags$head(tags$style("#next_ex_title{font-size: 20px;
-                       }
-                       #next_ex{font-size: 30px;
-                                font-style: bold;
-                       }
-                       #time_to_ex{font-size: 40px;
-                                font-style: bold;
-                       }
-                       "
-                       )),
+  br(),
+  includeCSS("styles.css"),
+  sidebarLayout(
+    sidebarPanel(width=4,
+      h3("Next exercise:"),
+      textOutput('next_ex'),
+      textOutput('time_to_ex'),
+      actionButton('start','Start'),
+      actionButton('stop','Stop'),
+      actionButton('reset','Reset'),
+      actionButton('skip','Skip'),
+      textOutput('completed')
+    ), 
+    
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Record results",
+                           br(),
+                           numericInput("bodyweight", "Bodyweight / kg", value=0, min=0, max=200, step=0.5),
+                           selectInput("record_ex", label="Exercise",
+                                       choices = tracked_exercises),
+                           radioButtons("record_hand", label="Hand",
+                                        choices = c("Left", "Right","Both"),
+                                        inline = TRUE),
 
-  #textOutput('time_remaining') # time  remaining
-  
-  selectInput("record_ex", label="Exercise",
-              choices = c("Front Lever", "Front Three", "Front Three", "Half Crimp", "Half Crimp", "Pinch Block")),
-  
-  radioButtons("record_hand", label="Hand",
-              choices = c("Left", "Right", "Both"), inline = TRUE),
-  
-  numericInput('record_time', 'Time / s:', value=0, min=0, max=10, step=0.5),
-  
-  numericInput('record_weight', 'Weight / kg:', value=0, min=-100, max=100, step=0.5),
-  actionButton("submit", "Submit"),
-
-  tableOutput('df'),
-  tableOutput('results_df'),
+                           numericInput('record_time', 'Time / s:',
+                                        value=0, min=0, max=10, step=0.5),
+                           numericInput('record_weight', 'Weight / kg:',
+                                        value=0, min=-100, max=100, step=0.5),
+                           actionButton("submit", "Submit")
+                  ),
+                  
+                  tabPanel("Graphs",
+                           br(),
+                           selectInput("ex_choice", label="Exercise",
+                                       choices = tracked_exercises),
+                           selectInput("metric_choice", label="Metric",
+                                       choices = c("Time", "Weight")),
+                           plotOutput("plot")
+                  ),
+                  
+                  tabPanel("Table",
+                           br(),
+                           p("Table of your results so far:"),
+                           DT::dataTableOutput('results_df')
+                  ),
+                  
+                  tabPanel("Settings",
+                           br(),
+                           sliderInput('interval', label = "Time between exercises / s:",
+                                        round = TRUE, value=240, min=20, max=400, step=10)
+                           )
+                  )
+      )
+  ) 
 )
 
 server <- function(input, output, session) {
   
+  # Initialize reactive values
+  counter <- reactiveVal(1)     # Tracks which exercise we are on
+  timer <- reactiveVal(10)      # Tracks time to next exercise 
+  active <- reactiveVal(FALSE)  # Tracks if timer is active or paused
   
-  # Use interval input to set up timings
-  reactive_df = reactive({
-    interval=input$interval
-    timings = c(10, interval, interval, rep(c(bh, interval-bh), 4),
-                            interval, rep(c(bh, interval-bh), 4), interval, 10)
-    df$timings <<- timings
-    df$cum_timings <<- cumsum(df$timings)
-    df
+  # observers for actionbuttons
+  observeEvent(input$start, {active(TRUE)})
+  observeEvent(input$stop, {active(FALSE)})
+  observeEvent(input$reset, {
+    timer(10)
+    counter(1)
+  })
+  observeEvent(input$skip, {timer(1)})
+  
+  # Dataframe that contains excercises and timings
+  rv = reactiveValues(
+    ex_df = tibble("exercise" = exercises, "hand" = hand,
+                   "date" = as.character(Sys.Date()),
+                   "timings" = NA)
+  )
+  
+  # Add timings as reactive (user can choose exercise interval)
+  observe({
+    bh = 20
+    i = input$interval
+    rv$ex_df$timings <- c(10, i, i, rep(c(bh, i-bh), 4),
+                          i, rep(c(bh, i-bh), 4), i, 10)})
+  
+  #### Handle Results ####
+  # Initialise results table
+  output$results_df <- DT::renderDataTable({read_sheet(sheet_URL)})
+  
+  observeEvent(input$submit, {
+    # Add data to google sheets
+    row = data.frame("exercise" = input$record_ex, "hand" = input$record_hand, 
+                     "time"=input$record_time, "weight"=input$record_weight,
+                     "bodyweight" = input$bodyweight, "date" = as.character(Sys.Date()))
+    sheet_append(sheet_URL, row)
+    
+    # After submission automatically add next exercise as defualt inputs
+    updateSelectInput(session, "record_ex",
+                      selected = rv$ex_df$exercise[counter()])
+    updateRadioButtons(session, "record_hand",
+                       selected = rv$ex_df$hand[counter()])
+    output$results_df <- DT::renderDataTable({read_sheet(sheet_URL)})
   })
   
-  # output$df <- renderTable({reactive_df()})
   
-  
-  # OG data
-  results_df = tibble("exercise" = character(), "hand" = character(),
-                      "time" = numeric(), "weight" = numeric(),
-                      "date" = as.character(Sys.Date()))
-  
-  # New data
-  reactive_results_df = eventReactive(input$submit,{
-    row = data.frame("excercises"=input$record_ex, "hand"=input$record_hand, 
-                     "time"=input$record_time, "weight"=input$record_weight)
-    results_df <<- rbind(results_df, row)
-    results_df
-    })
-  
-  #results_df = rbind(results_df, reactive_results_df())
-  output$results_df <- renderTable(reactive_results_df())
-
-  # Initialize reactive values
-  counter <- reactiveVal(1)  # Tracks which exercise we are on
-  timer <- reactiveVal(10)
-  active <- reactiveVal(FALSE)
+  #### Plot results ####
+  output$plot = renderPlot({
+    df = read_sheet(sheet_URL)
+    df$date = ymd(df$date)
+    df$hand <- as.factor(df$hand)
+    colours = c("black", "goldenrod3", "darkslategray4")
+      
+    if (input$metric_choice == "Time"){
+      p = df %>%
+        filter(exercise == input$ex_choice) %>%
+        ggplot(aes(date, time, color = hand, size=weight)) +
+        geom_point(alpha=0.6) +
+        scale_color_manual(values=colours) +
+        scale_size("weight", range = c(1,5))
+    }
+    
+    if (input$metric_choice == "Weight"){
+      p = df %>%
+        filter(exercise == input$ex_choice) %>%
+        ggplot(aes(date, weight, color = hand, size=time)) +
+        geom_point(alpha=0.6) +
+        scale_color_manual(values=colours) +
+        scale_size("weight", range = c(1,5))
+    }
+    p
+  })
   
   # Show exercise and time until the hang
-  output$next_ex_title <- renderText({"Next exercise:"})
-  output$next_ex <- renderText({exercise[counter()]})
+  output$next_ex <- renderText({
+    hand_or_hands <- if (rv$ex_df$hand[counter()] == "Both") "hands" else "hand"
+    text = sprintf("%s (%s %s)", rv$ex_df$exercise[counter()], rv$ex_df$hand[counter()], hand_or_hands)})
   output$time_to_ex <- renderText({paste(seconds_to_period(timer()))}) #####
   
-  # observer that invalidates every second. If timer is active, decrease by one.
+  # Show proportion of exercises completed
+  output$completed <- renderText(sprintf("Completed %s/%s", counter()-1, length(exercises)-1))
+  
+  #### Timer (observer that invalidates every second) ####
   observe({
-    invalidateLater(500, session)
-    
+    invalidateLater(1000, session)
     isolate({
       if(active()){
         
         timer(timer()-1)
         
         # Add 5 beep coundown to hang
-        if (timer()<6 & timer()>0){
+        if (timer()<=5 & timer()>0){
           beep(sound = 10)   # 5 beeps to countdown to hang.
         }
         
@@ -120,9 +194,10 @@ server <- function(input, output, session) {
         else if (timer()==0){
           beep(sound = 1)
           counter(counter()+1)
-          timer(df$timings[counter()])
+          timer(rv$ex_df$timings[counter()])
           
-          if (counter()==length(exercise)){
+          # Check if workout finished
+          if (counter()==length(exercises)){
             beep(sound = 4)
             Sys.sleep(1)
             beep(sound = 3)
@@ -131,7 +206,7 @@ server <- function(input, output, session) {
               title = "Workout Completed!",
               "Countdown completed!"))
           }
-        } else if (counter()>6 & timer() > (df$timings[counter()]-11)){ 
+        } else if (timer() > (rv$ex_df$timings[counter()+1]-11)){ 
             beep(sound = 2)  # Beeps to count hangtime
           }
         }
@@ -139,19 +214,6 @@ server <- function(input, output, session) {
     )
   })
   
-
-  # observers for actionbuttons
-  
-  observeEvent(input$start, {active(TRUE)})
-  observeEvent(input$stop, {active(FALSE)})
-  observeEvent(input$reset, {timer(10)
-    counter(1)})
-  observeEvent(input$submit, {output$results_df <- renderTable({reactive_results_df()})})
-  
-  
 }
-
-colnames(results_df)
-
 
 shinyApp(ui, server)
